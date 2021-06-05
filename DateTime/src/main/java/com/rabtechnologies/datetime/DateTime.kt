aff3b5b48bceebfe047df033538d04fe363f9766
alianshad123 <anshad993@gmail.com>
2021-06-05 19:42:01 +0530

    object update

--- DateTime/src/main/java/com/rabtechnologies/datetime/DateTime.kt
+++ DateTime/src/main/java/com/rabtechnologies/datetime/DateTime.kt
@@ -7,8 +7,9 @@ import android.widget.Toast
 
 
 class DateTime {
-
-    fun showToast(c: Context?, message: String?) {
-        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    companion object {
+        fun showToast(c: Context?, message: String?) {
+            Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
