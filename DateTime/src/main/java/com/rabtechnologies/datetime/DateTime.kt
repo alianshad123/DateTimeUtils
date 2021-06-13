91b5dcc35ac04f4ae8b34a073e641dcf796205d5
alianshad123 <anshad993@gmail.com>
2021-06-13 22:16:07 +0530

    date format update

--- DateTime/src/main/java/com/rabtechnologies/datetime/DateTime.kt
+++ DateTime/src/main/java/com/rabtechnologies/datetime/DateTime.kt
@@ -1,15 +1,38 @@
 package com.rabtechnologies.datetime
 
-import android.content.Context
-import android.widget.Toast
-
-
-
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DateTime {
     companion object {
-        fun showToast(c: Context?, message: String?) {
-            Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+        fun getCurrentUTCDate(format:String?,timeZone:String?): String {
+            val sdf = SimpleDateFormat(format, Locale.getDefault())
+            sdf.timeZone = TimeZone.getTimeZone(timeZone)
+            return sdf.format(Date())
+        }
+
+        fun getCurrentDate(format:String?): String? {
+            val simpleDateFormat = SimpleDateFormat(format)
+            return simpleDateFormat.format(Date())
+        }
+
+        fun getCurrentTime(format:String?): String? {
+            val simpleDateFormat = SimpleDateFormat(format)
+            return simpleDateFormat.format(Date())
+        }
+
+        fun getFormatedDate(date: String?,currentFormat: String?, expectedFormat: String?): String? {
+            val format = SimpleDateFormat(currentFormat)
+            val targetFormat = SimpleDateFormat(expectedFormat)
+            val date: Date = format.parse(date)
+            return targetFormat.format(date)
+        }
+
+        fun getFormatedTime(time: String?,currentFormat: String?, expectedFormat: String?): String? {
+            val format = SimpleDateFormat(currentFormat)
+            val targetFormat = SimpleDateFormat(expectedFormat)
+            val date: Date = format.parse(time)
+            return targetFormat.format(date)
         }
     }
 }
